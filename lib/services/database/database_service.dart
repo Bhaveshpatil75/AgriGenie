5ef795220be0302cfd1f7721153b99d5c1b0ac1b
Bhavesh Patil <bhaveshpatil7504@gmail.com>
2024-10-02 11:24:33 +0530

    CRUD done on farms and crops

--- lib/services/database/database_service.dart
+++ lib/services/database/database_service.dart
@@ -10,7 +10,7 @@ class DatabaseService{
    Future<void> addFarmer(Map<String,dynamic> map)async{
       try {
          uid != null ?
-         await farmerDB.doc(uid!).collection("questions").doc("answers").set(map)
+         await farmerDB.doc(uid).collection("questions").doc("answers").set(map)
              : throw Exception();
          log("data saved");
       }catch (e){
@@ -18,27 +18,74 @@ class DatabaseService{
       }
    }
    Future<Object?> getFarmeranswers()async{
-      DocumentSnapshot snapshot=await farmerDB.doc(uid!).collection("questions").doc("answers").get();
+      DocumentSnapshot snapshot=await farmerDB.doc(uid).collection("questions").doc("answers").get();
       return snapshot.data();
    }
    Future<void> addFarm(String name,Map<String,dynamic> map)async{
       try{
          if (uid!=null){
-            await farmerDB.doc(uid!).collection("farms").doc(name).set(map);
+            await farmerDB.doc(uid).collection("farms").doc(name).set(map);
          }else{
             throw Exception();
          }
-
       }catch(e){
          log(e.toString());
       }
    }
    Future<List<dynamic>> getFarms()async{
-      QuerySnapshot querySnapshot=await farmerDB.doc(uid!).collection("farms").get();
+      QuerySnapshot querySnapshot=await farmerDB.doc(uid).collection("farms").get();
       return querySnapshot.docs.map((doc)=>doc.id).toList();
    }
    Future<Object?> getAfarm(String name)async{
-      DocumentSnapshot snapshot=await farmerDB.doc(uid!).collection("farms").doc(name).get();
+      DocumentSnapshot snapshot=await farmerDB.doc(uid).collection("farms").doc(name).get();
       return snapshot.data();
    }
+   Future<void> deleteFarm(String name)async{
+      try{
+         await farmerDB.doc(uid).collection("farms").doc(name).delete();
+      }catch(e){
+         log("Cannot delete farm");
+      }
+   }
+   Future<void> editFarm(String oldName,String newName,Map<String,dynamic> map)async{
+      try{
+         await deleteFarm(oldName);
+         await addFarm(newName, map);
+      }catch(e){
+         log("Error while editing...");
+      }
+   }
+   Future<void> addCrop(String farmName,String name,Map<String,dynamic> map)async{
+      try{
+         if(uid!=null){
+            await farmerDB.doc(uid).collection("farms").doc(farmName).collection("crops").doc(name).set(map);
+         }else throw Exception();
+      }catch(e){
+         log("Error adding CROP");
+      }
+   }
+   Future<List<dynamic>>getCrops(String farmName)async{
+      QuerySnapshot querySnapshot=await farmerDB.doc(uid).collection("farms").doc(farmName).collection("crops").get();
+      return querySnapshot.docs.map((doc)=>doc.id).toList();
+   }
+   Future<Object?>getAcrop(String farmName,String cropName)async{
+      DocumentSnapshot documentSnapshot=await farmerDB.doc(uid).collection("farms").doc(farmName).collection("crops").doc(cropName).get();
+      return documentSnapshot.data();
+   }
+   Future<void> deleteCrop(String farmName,String cropName)async{
+      try{
+         await farmerDB.doc(uid).collection("farms").doc(farmName).collection("crops").doc(cropName).delete();
+      }catch(e){
+         log("Cannot delete CROP");
+      }
+   }
+   Future<void> editCrop(String farmName,String oldName,String newName,Map<String,dynamic> map)async{
+      try{
+         await deleteCrop(farmName,oldName);
+         await addCrop(farmName,newName, map);
+      }catch(e){
+         log("Error while editing...");
+      }
+   }
+
 }
